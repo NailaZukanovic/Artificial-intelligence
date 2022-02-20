4e15a68a40f8f5178dfdf2f599449e50a44f6d39
Naila Zukanovic <86067688+NailaZukanovic@users.noreply.github.com>
2022-02-20 10:41:12 +0100

    Update asgi.py

--- pytorch_django/asgi.py
+++ pytorch_django/asgi.py
@@ -1,16 +1,17 @@
 """
-ASGI config for pytorch_django project.
+ASGI config for Project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pytorch_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
 
 application = get_asgi_application()
+
